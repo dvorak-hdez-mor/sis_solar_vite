54d5f7753fa69d1bb933677559a2a7cd2af2dbec
dvorak-hdez-mor <miguel.hernandezmor@alumno.buap.mx>
2022-08-27 20:11:43 -0500

    Agregada luna

--- main.js
+++ main.js
@@ -28,14 +28,31 @@ camera.position.setZ(30);
 // alternativa de agregar cone
 const cone = new THREE.Mesh(
 	new THREE.ConeGeometry(5, 10, 4),
-	new THREE.MeshStandardMaterial({color: 0xff00ff})
+	new THREE.MeshLambertMaterial({color: 0xff00ff})
 );
+cone.receiveShadow = true;
 scene.add(cone);
+
+const cube = new THREE.Mesh(
+	new THREE.BoxGeometry(5, 5, 5),
+	new THREE.MeshMatcapMaterial({color: 0x00ff00})
+);
+
+//cube.material.transparent = true;
+//cube.material.opacity = 0.5;
+scene.add(cube);
+
+const torusKnot = new THREE.Mesh(
+	new THREE.TorusKnotGeometry(2, 0.5, 20, 5),
+	new THREE.MeshLambertMaterial({color: 0xf0f0f0})
+);
+torusKnot.receiveShadow = true;
+scene.add(torusKnot);
 
 // agregando luz blanca (0xffffff)
 const pointLight = new THREE.PointLight(0xffffff);
 // posicion del punto de luz
-//pointLight.position.set(5, 5, 5);
+//pointLight.position.set(0, 6, 0);
 scene.add(pointLight);
 
 const ambientLigth = new THREE.AmbientLight(0xffffff);
@@ -55,7 +72,8 @@ const spaceTexture = new THREE.TextureLoader().load('./space.jpg');
 scene.background = spaceTexture;
 
 // pasos para movimiento de cone
-var step = 0;
+var stepCone = 0;
+var stepTorus = 0;
 
 // loop infinito
 function animate() {
@@ -66,9 +84,13 @@ function animate() {
 	cone.rotation.y += 0.01;
 	cone.rotation.z += 0.01;
 
-	step += 0.1;
-	cone.position.x = 20*Math.sin(step);
-	cone.position.z = 20*Math.cos(step);
+	stepCone += 0.01;
+	cone.position.x = 30*Math.sin(stepCone);
+	cone.position.z = 30*Math.cos(stepCone);
+
+	stepTorus += 0.07;
+	torusKnot.position.x = cone.position.x-10*Math.sin(stepTorus);
+	torusKnot.position.z = cone.position.z-10*Math.cos(stepTorus);
 
 	controls.update();
 
